c373d43a0189fd30f4c6f3c22cb24a167246c53f
Indukakapudu2002 <126580129+Indukakapudu2002@users.noreply.github.com>
2023-03-01 18:21:51 +0530

    Create arraylist.java

---
+++ javapractice/arraylist.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class arraylists
+{
+    public static void main(String args[])
+    {
+        /*ArrayList al=new ArrayList();
+        //ArrayList <String>al1=new <String> ArrayList();
+        al.add(1);
+        al.add("hi");
+        System.out.println(al);
+        System.out.println("number of elements in arraylist is "+al.size());
+        al.add(3);
+        al.add(1);
+        System.out.println(al);
+        System.out.println("number of elements in arraylist is "+al.size());
+        al.remove(1);
+        System.out.println(al);
+        System.out.println("number of elements in arraylist is "+al.size());
+        al.add(3,"welcome");
+        System.out.println(al);
+        System.out.println(al.get(2));
+        al.set(3,"hi");
+        System.out.println(al);
+        System.out.println(al.contains(1));
+        System.out.println(al.isEmpty());
+        System.out.println("the elements in arraylist is");
+        for(int i=0;i<al.size();i++)
+        {
+            System.out.println(al.get(i));
+        }
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        al.add(1);
+        al.add(2220);
+        al.add(3);
+        al.add(64);
+        al.add(4556);
+        System.out.println("elements before sorting");
+        for(int i=0;i<al.size();i++)
+        {
+            System.out.println(al.get(i));
+        }
+        Collections.sort(al);
+        System.out.println("elements after sorting");
+        for(int i=0;i<al.size();i++)
+        {
+            System.out.println(al.get(i));
+        }*/
+        String a[]={"cat","dog","pig"};
+        ArrayList al=new ArrayList(Arrays.asList(a));
+        System.out.println(al);
+    }
+}
